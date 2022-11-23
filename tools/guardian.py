a880d78d7469a922ebd2fa032438271e935349d1
sborto86 <sborto81@gmail.com>
2022-11-23 09:45:03 +0100

    First commit

---
+++ tools/guardian.py
@@ -0,0 +1,114 @@
+## IMPORTING LIBRARIES
+if "requests" not in dir():
+    import requests
+if "urllib" not in dir ():
+    import urllib.parse
+from dotenv import load_dotenv
+if "os" not in dir():
+    import os
+if "datetime" not in dir():
+    import datetime
+    from dateutil.relativedelta import relativedelta
+from unidecode import unidecode
+from tqdm import tqdm
+
+##### GETTING PASSWORDS
+
+env_path = (os.path.join("", ".env"))
+load_dotenv(env_path)
+GP = os.getenv("THE_GUARDIAN")
+
+def get_guardian_articles(query, datefrom=None, dateto=None):
+    '''
+    Function that get the number of articles published in The Guardian the last 2 years for a given query
+    Arguments:
+        query: str. 
+                query to retreive the article number
+        datefrom(optional): str. (format: YYYY-DD-MM)
+        dateto(optional): str. (format: YYYY-DD-MM)
+    Returns: list of dictionaries
+    '''
+    articles = []
+    if not dateto:
+        dto = datetime.datetime.now() - relativedelta(years=2)
+        dateto = f"{dto.year}-{dto.month}-{dto.day}"
+    else:
+        d_ls = dateto.split("-")
+        dto = datetime.datetime(int(d_ls[0]), int(d_ls[1]), int(d_ls[2]))
+    if not datefrom:
+        dfr = datetime.datetime.now()
+        datefrom = f"{dfr.year-2}-{dfr.month}-{dfr.day}"
+    else:
+        d_ls = datefrom.split("-")
+        dfr = datetime.datetime(int(d_ls[0]), int(d_ls[1]), int(d_ls[2]))    
+    url = 'https://content.guardianapis.com/search?'
+    parameters = {
+        'api-key': GP,
+        'q': query,
+        'from-date': datefrom,
+        'to-date': dateto,
+        'order-by': 'newest',
+        'page':1,
+        'use-date':'published',
+        'page-size': 100
+    }
+    url2 = url + urllib.parse.urlencode(parameters)
+    try:
+        response = requests.get(url2)
+    except:
+        print("Network error please check your internet connection")
+    if response.status_code == 200:
+        response = response.json()
+        pages = response['response']['pages']
+        #check if the number of pages are more than the number of days
+        if pages > (dto-dfr).days:
+            day = dfr
+            for loop in tqdm(range((dfr-dto).days+1)):
+                parameters['from-date'] = f"{day.year}-{day.month}-{day.day}"
+                parameters['to-date'] = f"{day.year}-{day.month}-{day.day}"
+                day = day + datetime.timedelta(days=1)
+                url2 = url + urllib.parse.urlencode(parameters)            
+                try:
+                    response = requests.get(url2)
+                    if response.status_code == 200:
+                        response = response.json()
+                        dic_ = { "date": f"{day.year}-{day.month}-{day.day}",
+                                "articles": response['response']['total'],
+                                "query": query
+                        }
+                        articles.append(dic_)
+                    else: 
+                        print(f"Unable to fully retrieve articles from {query}, stopped at {day.year}-{day.month}-{day.day}: Error {response.status_code}: {response.text}")
+                        return articles
+                except:
+                        print("Network error please check your internet connection, stopped at {day.year}-{day.month}-{day.day}")
+                        return articles
+        else:
+            for i in tqdm(range(2, pages+1)):
+                parameters['page'] = i
+                url2 = url + urllib.parse.urlencode(parameters)
+                try:
+                    response = requests.get(url2)
+                    if response.status_code == 200:
+                        response = response.json()
+                        results = response['response']['results']
+                        print(response['response'])
+                        dic_dates = {}
+                        for e in results:
+                            pubdate = e['webPublicationDate'].split("T")[0]
+                            if pubdate in dic_dates:
+                                dic_dates[pubdate] +=1
+                            else:
+                                dic_dates[pubdate] =1
+                        for dat, value in dic_dates.items():
+                            art = {
+                                "date": dat,
+                                "articles": value,
+                                "query": query
+                            }
+                            articles.append(art)
+                    else: 
+                        print(f"Unable to retrieve articles from page {i} of {query}: Error {response.status_code}: {response.text}")
+                except:
+                    print("Network error please check your internet connection, Unable to retrieve articles from page {i} of {query}")
+    return articles
